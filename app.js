b6de9ef2113232d205fd5b324c5a997a34b0aa8e
haydent7 <hterek@muni-link.com>
2024-08-29 16:34:29 -0400

    Added html components and so javascripting to use the button. Attempting to fetch and print api data in console.

---
+++ app.js
@@ -0,0 +1,31 @@
+window.onload = function(event) {
+    fetch('https://api.nhle.com/stats/rest/en/team')
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Could not connect to API');
+            }
+
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error: ', error);
+        });
+}
+
+function dropdownFunction() {
+    document.getElementById("teamDropdownMenu").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if(!event.target.matches('.dropdownBtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for(var i = 0; i < dropdowns.length; i++) {
+            if(dropdowns[i].classList.contains('show')) {
+                dropdowns[i].classList.remove('show');
+            }
+        }
+    }
+}
