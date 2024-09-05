bd99b3560d290aedeedbc63d7c0278167ff4babb
haydent7 <hterek@muni-link.com>
2024-09-05 09:02:32 -0400

    adding cors mode to test in browser

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 window.onload = function(event) {
-    fetch('https://api.nhle.com/v1/schedule/2023-09-23')
+    fetch('https://api.nhle.com/stats/rest/en/team', {
+        mode: 'cors',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
         .then(response => {
             if(!response.ok) {
                 throw new Error('Could not connect to API');
